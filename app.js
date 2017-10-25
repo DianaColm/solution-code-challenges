b6ef55c4b296e32f7be26cbcea07565235f38a00
DianaColm <dcolmenerog@yahoo.com.mx>
2017-10-25 12:40:08 -0500

    add code

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var name = prompt("escribe tu nombre");
+function findShortestOfThreeWords(word1, word2, word3){
+  var leng1 = word1.length;
+  var leng2 = word2.length;
+  var leng3 = word3.length;
+
+  if(leng1 <= leng2 && leng1 <= leng3){
+    return word1;
+  }else if(leng2 < leng1 && leng2 <= leng3){
+    return word2;
+  }else{
+    return word3;
+  }
+}
